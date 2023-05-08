cc6e687280e092e29ab6e10b6778e3612eff4003
Nico Guerin <guerinnico08@gmail.com>
2023-05-08 08:45:27 +0200

    S'han afegit els fitxers i realitzat el codi que mostra els requeriments

--- main.cc
+++ main.cc
@@ -1,22 +1,79 @@
 #include <iostream>
+#include <string>
 #include <map>
 #include <fstream>
 using namespace std;
 
-bool esCaracterValid(char c) {
-    return (c>='a' && c<='z') || (c>='A' && c<='Z') || c==',' || c=='.' || c==';';
+void mostraMenu() {
+    cout << "1. Llegir paraules d'un fitxer" << endl;
+    cout << "2. Llegir lletres d'un fitxer" << endl;
+    cout << "3. Sortir" << endl;
+}
+
+bool esSeparador(char c) {
+    return c==' ' or c==';' or c==':' or c==',' or c=='.' or c=='\n';
+}
+
+string generaRutaFitxer() {
+    string ubicacioFitxer = "./fitxers/", nomFitxer;
+
+    cout << "Introdueix la ruta del fitxer (nom_fitxer.txt): ";
+    cin >> nomFitxer;
+    ubicacioFitxer += nomFitxer;
+
+    return ubicacioFitxer;
+}
+
+void llegeixParaulesFitxer(ifstream& f, map<string, int>& freq) {
+    string cadenaAct = "";
+    while (f) {
+        char act = f.get();
+        if (esSeparador(act)) {
+            if (cadenaAct!="") {
+                ++freq[cadenaAct];
+                cadenaAct = "";
+            }
+        }
+        else cadenaAct += act;
+    }
+}
+
+void llegeixLletresFitxer(ifstream& f, map<char, int>& freq) {
+    while (f) {
+        char act = f.get();
+        ++freq[act];
+    }
 }
 
 int main() {
-    ifstream f("./fitxer.txt");
-    map<char, int> freq;
+    int opcio = 0;
+    while (opcio!=3) {
+        mostraMenu();
 
-    if (f.is_open()) {
-        while (f) {
-            char act = f.get();
-            if (esCaracterValid(act)) ++freq[act];
+        cout << "Introdueix una opcio: ";
+        cin >> opcio;
+
+        if (opcio==3) cout << "Sortint del programa..." << endl;
+        else {
+            string ubicacioFitxer = generaRutaFitxer();
+            ifstream f(ubicacioFitxer);
+
+            if (!f) cout << "No s'ha pogut obrir el fitxer" << endl;
+            else {
+                if (opcio==1) {
+                    map<string, int> freq;
+                    llegeixParaulesFitxer(f, freq);
+                    for (auto it : freq) cout << it.first << ": " << it.second << endl;
+                }
+                else if (opcio==2) {
+                    map<char, int> freq;
+                    llegeixLletresFitxer(f, freq);
+                    for (auto it : freq) cout << it.first << ": " << it.second << endl;
+                }
+
+                f.close();
+                cout << endl;
+            }
         }
     }
-    
-    for (auto it : freq) cout << it.first << ": " << it.second << endl;
 }
